da3e1d511f4065d2997772d5be483f1de884cfd5
hnv1212 <h19982312@gmail.com>
2023-12-30 19:06:39 +0700

    part28: health check

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { LoggerModule } from '@app/common';
+import { HealthModule, LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
@@ -20,7 +20,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION: Joi.string().required(),
         HTTP_PORT: Joi.number().required(),
-        TCP_PORT: Joi.number().required()
+        TCP_PORT: Joi.number().required(),
       }),
     }),
     JwtModule.registerAsync({
@@ -30,9 +30,9 @@ import { JwtStrategy } from './strategies/jwt.strategy';
           expiresIn: `${configService.get('JWT_EXPIRATION')}s`,
         },
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
-    
+    HealthModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
