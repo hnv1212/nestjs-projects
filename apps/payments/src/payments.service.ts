a6dbc5ff9c435e484c027bd11b0f4fa0312720c2
hnv1212 <h19982312@gmail.com>
2023-12-30 16:42:10 +0700

    part19: Email Notification

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -34,7 +34,10 @@ export class PaymentsService {
       currency: 'usd',
     });
 
-    this.notificationsSevice.emit('notify_email', { email });
+    this.notificationsSevice.emit('notify_email', {
+      email,
+      text: `Your payment of $${amount} has completed successfully`,
+    });
 
     return paymentIntent;
   }
