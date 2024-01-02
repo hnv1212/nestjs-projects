daea16b2f124eec2cf42373d16ad07f140c931d5
hnv1212 <h19982312@gmail.com>
2024-01-02 19:15:57 +0700

    part53: Error Handling: Username conflicts

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { Repository, EntityRepository } from 'typeorm';
@@ -7,6 +11,16 @@ export class UsersRepository extends Repository<User> {
   async createUser(dto: AuthCredentialsDto): Promise<void> {
     const { username, password } = dto;
     const user = await this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
