256db4b01ae28ce1017a0932d70d0ec31e021dd2
hnv1212 <h19982312@gmail.com>
2024-01-02 19:10:44 +0700

    part51: Feature: Signing up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,12 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { Repository, EntityRepository } from 'typeorm';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(dto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = dto;
+    const user = await this.create({ username, password });
+    await this.save(user);
+  }
+}
