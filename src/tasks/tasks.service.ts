ffe2c8bf07279df71a2804be97bdcfaf2b81b3bc
hnv1212 <h19982312@gmail.com>
2024-01-02 18:20:31 +0700

    part40: Creating a Tasks Repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
+  getAllTasks(): Task[] {}
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
     
+  }
+
+  getTaskById(id: string): Task {}
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+  }
+
+  deleteTask(id: string): void {
+
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+
+  }
 }
