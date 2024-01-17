91b55813d0ca998669735be4aa37952a0142134b
hnv1212 <h19982312@gmail.com>
2024-01-17 14:41:03 +0700

    part46: Schema Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       // ignoreEnvFile: true => use when using heroku env?
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
