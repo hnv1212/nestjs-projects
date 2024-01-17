f98c5687aa9a7669742adbb0024342255a21f1be
hnv1212 <h19982312@gmail.com>
2024-01-17 15:26:26 +0700

    part50: Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,18 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       // ignoreEnvFile: true => use when using heroku env?
@@ -20,19 +32,9 @@ import appConfig from './config/app.config';
         DATABASE_HOST: Joi.required(),
         DATABASE_PORT: Joi.number().default(5432),
       }),
-      load: [appConfig]
+      load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     CoffeeRatingModule,
     DatabaseModule,
   ],
