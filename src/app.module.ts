cdab4f119d357e1b367c74f69d470afebf36d112
hnv1212 <h19982312@gmail.com>
2024-01-19 18:54:31 +0700

    part69: Diving Into e2e Tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import Joi from '@hapi/joi';
+import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 import { APP_PIPE } from '@nestjs/core';
 import { CommonModule } from './common/common.module';
